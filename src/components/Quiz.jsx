4f2cca4e1fc18ea46bf3bd8ef8910f830b66788d
ponnparamaguru <ponnparamaguru8.com>
2024-04-26 18:52:47 +0530

    second commit

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import {Data} from '../data/Data'
+
+const QuizApp = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [score, setScore] = useState(0);
+  const [quizCompleted, setQuizCompleted] = useState(false); 
+
+
+  const handleOptionSelect = (optionIndex) => {
+    setSelectedOption(optionIndex);
+  };
+
+  const handleNextQuestion = () => {
+    
+    if (selectedOption === Data[currentQuestion].answer) {
+      setScore(score + 1);
+    }
+    
+    setSelectedOption(null);
+    setCurrentQuestion(currentQuestion + 1);
+
+    if (currentQuestion === Data.length - 1) {
+      setQuizCompleted(true);
+    }
+  };
+
+  return (
+    <div className='w-full h-screen bg-yellow-300 relative flex justify-center items-center'>
+    
+      {currentQuestion < Data.length ? (
+        <div className='bg-yellow-50 md:w-7/12 md:h-5/6 h-5/6 w-11/12 rounded-3xl border-4 border-black relative'>
+            <div className='p-10'>
+            <h1 className='md:text-6xl text-4xl'>Q{currentQuestion+1}:<span className='md:text-4xl text-3xl'>{" "+Data[currentQuestion].question}</span></h1>
+          <div className='flex justify-center'>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 bottom-24 absolute">
+            {Data[currentQuestion].options.map((option, i) => (
+              <button
+                key={i}
+                className={`text-xl capitalize rounded-xl border-4 border-black px-20 py-5 flex justify-center items-center ${selectedOption === i+1 ? 'bg-green-200  element-click':'bg-violet-200 element'}`}
+                onClick={() => handleOptionSelect(i+1)}
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+          </div>
+          <button
+            className='w-24 h-10 bg-green-300 text-xl rounded-lg border-4 border-black element flex justify-center items-center absolute right-5 bottom-5'
+            onClick={handleNextQuestion}
+          >
+            Next
+          </button>
+          </div>
+        </div>
+      ) : (
+        <div className='bg-yellow-50 md:w-7/12 md:h-5/6 h-5/6 w-11/12 rounded-3xl border-4 border-black flex justify-center items-center'>
+            <div className='text-center'>
+                <h1 className="text-3xl font-bold mb-4">Quiz Completed! ⏳</h1>
+                <h1 className="text-6xl font-bold mb-4">{score}/{Data.length}</h1>
+                <h1 className="text-2xl font-bold mb-4">Your General Knowledge is {score/Data.length*100 > 50 ? "Awsome 🤩" : "Need to be Improved 🥲"} </h1>
+            </div>
+        </div>
+      )}
+
+      {currentQuestion < Data.length && !quizCompleted && (
+        <div className='absolute top-10 md:right-60 right-10'>
+          <div className='w-20 h-20 bg-pink-300 relative'>
+            <div className='w-20 h-20 bg-pink-300 absolute' style={{ transform: "rotate(30deg)"}}>
+              <div className='w-20 h-20 bg-pink-300 absolute flex justify-center items-center' style={{ transform: "rotate(30deg)"}}>
+                <h1 className='text-5xl' style={{ transform: "rotate(-60deg)"}}>{score}</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      
+    </div>
+  );
+};
+
+export default QuizApp;
---
+++ src/data/Data.jsx
@@ -0,0 +1,27 @@
+export const Data = [
+    {
+        question: "How many days do we have in a week?",
+        options:["one", "four", "seven","eight"],
+        answer: 3
+    },
+    {
+        question: "Which animal is known as the ‘Ship of the Desert?",
+        options:["camel", "graffie", "lion","panther"],
+        answer: 1
+    },
+    {
+        question: "Which king of the jungle?",
+        options:["cheetha", "tiger", "lion","camel"],
+        answer: 3
+    },
+    {
+        question: "Which festival is called the festival of light?",
+        options:["Diwali", "Pongal", "Ramzan","Christmas"],
+        answer: 1
+    },
+    {
+        question: "In which direction does the sunrise?",
+        options:["west", "east", "south","north"],
+        answer: 2
+    },
+]
